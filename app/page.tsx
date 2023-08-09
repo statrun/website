d4bd9ccd97053a7e196ac67e2f469df3624fcc6e
ysm-dev <i@ysm.dev>
2023-08-09 10:09:45 +0800

    remove scroll

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      Soon...
+    <main className='flex flex-col h-full items-center justify-between p-24'>
+      Soon....
     </main>
   )
 }
